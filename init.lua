b67c095a33626593a4b63bd2b99f1f2b8878fd56
Jai Verma <jai2.verma@outlook.com>
2023-06-25 13:09:59 +0530

    Adding initial keymaps

--- init.lua
+++ init.lua
@@ -1,11 +1,1 @@
-vim.opt.showcmd = true
-vim.opt.laststatus = 2
-vim.opt.cursorline = true
-vim.opt.autoread = true
-
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 2
-vim.opt.shiftround = true
-vim.opt.expandtab = true
-vim.opt.hlsearch = true
-vim.opt.number = true
+require("core.keymaps")
