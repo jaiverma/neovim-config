c795a821abe6ea423fc9ca10b4a68468431d628a
Jai Verma <jai2.verma@outlook.com>
2023-06-28 13:42:54 +0530

    Setup nvim-lsp with mason

---
+++ lua/core/plugin_config/lsp_config.lua
@@ -0,0 +1,11 @@
+require('mason').setup()
+require('mason-lspconfig').setup()
+
+local on_attach = function(_, _)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+  vim.keymap.set('n', '<leader>ca', vim.lsb.buf.code_action, {})
+  vim.keymap.set('n', 'gd', vim.lsb.buf.definition, {})
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+  vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+end
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -26,6 +26,11 @@ return require('packer').startup(function(use)
     'nvim-lualine/lualine.nvim',
     requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
+  use {
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig'
+  }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
