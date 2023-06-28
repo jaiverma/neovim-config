d2bc8d02e9301fa79cfff8cbae222efed535baa0
Jai Verma <jai2.verma@outlook.com>
2023-06-28 16:23:23 +0530

    Setting up ocaml-lsp

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,11 +1,15 @@
 require('mason').setup()
 require('mason-lspconfig').setup()
 
-local on_attach = function(_, _)
+local on_attach = function(_, bufnr)
   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
-  vim.keymap.set('n', '<leader>ca', vim.lsb.buf.code_action, {})
-  vim.keymap.set('n', 'gd', vim.lsb.buf.definition, {})
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
   vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 end
+
+require('lspconfig').ocamllsp.setup {
+  on_attach = on_attach
+}
