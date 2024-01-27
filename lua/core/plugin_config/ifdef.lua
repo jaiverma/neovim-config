70a4986f5066310491f491ea94e0824dafaa1141
Jai Verma <jai2.verma@outlook.com>
2024-01-27 11:19:21 -0800

    Updating ifdef context to display identifier and state

--- lua/core/plugin_config/ifdef.lua
+++ lua/core/plugin_config/ifdef.lua
@@ -15,7 +15,16 @@ function get_ifdef()
     if expr:type() == "preproc_else" then
       break
     elseif expr:type() == "preproc_ifdef" then
-      is_defined = true
+      -- child 0 is the ifdef node
+      local ifdef_type = expr:child(0):type()
+      -- if #ifdef, then 'defined'
+      -- if #ifndef, then 'not defined'
+      if ifdef_type == "#ifdef" then
+        is_defined = true
+      elseif ifdef_type == "#ifndef" then
+        is_defined = false
+      end
+
       break
     end
     expr = expr:parent()
@@ -33,14 +42,45 @@ function get_ifdef()
       if expr:type() == "preproc_ifdef" then
         break
       end
+      expr = expr:parent()
     end
-    expr = expr:parent()
   end
 
-  print(expr)
-  local start_row, _, _ = expr:start()
-  local ifdef_line = vim.api.nvim_buf_get_lines(vim.api.nvim_get_current_buf(), start_row, start_row + 1, true)
-  x = table.concat(ifdef_line, "\n")
+  -- print(expr:type())
+  -- if we have a preproc_ifdef TSNode, then it will have a named identifer as a child
+  -- which will contain the name of the ifdef
+  local ifdef_node = nil
+  if expr:child_count() > 1 then
+    -- child 1 is the identifier in the ifdef statement
+    local ident = expr:child(1)
+    if ident:type() == "identifier" then
+      ifdef_node = ident
+      -- print(ident:type())
+    end
+  end
+
+  if not ifdef_node then
+    return ""
+  end
+
+  local start_row, start_col, end_row, end_col = ifdef_node:range()
+  local lines = vim.api.nvim_buf_get_lines(vim.api.nvim_get_current_buf(), start_row, end_row + 1, false)
+
+  if start_row ~= end_row then
+    -- handle multiline ifdef statement
+    if #lines == 0 then
+      return ""
+    end
+    -- fix first line to start from start_col
+    -- and last line to end at end_col
+    lines[1] = string.sub(lines[1], start_col)
+    lines[#lines] = string.sub(lines[#lines], 1, end_col)
+  else
+    -- we can only have 1 line in this condition
+    lines[1] = string.sub(lines[1], start_col, end_col)
+  end
+
+  local ifdef_name = table.concat(lines, "\n")
 
   local label = ""
   if is_defined then
@@ -49,7 +89,7 @@ function get_ifdef()
     label = "no"
   end
 
-  print(string.format("%s: %s", x, label))
+  print(string.format("%s: %s", ifdef_name, label))
   return x
 end
 
